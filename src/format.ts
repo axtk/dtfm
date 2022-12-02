31ec7dcd9c3a9d28d35c38d9df01fc3bc346c368
axtk <axtkx@inbox.ru>
2022-12-03 02:58:18 +0300

    update deps, update fill types

--- src/format.ts
+++ src/format.ts
@@ -6,8 +6,6 @@ import {INVALID_DATE_STRING} from './const';
 import {isInvalidDate} from './isInvalidDate';
 import {getDateComponents} from './getDateComponents';
 
-type FillTransformMap = Parameters<typeof fill>[2];
-
 export function format(
     date: DateValue,
     dateTemplate: string,
@@ -21,5 +19,5 @@ export function format(
     if (!dateComponents)
         return INVALID_DATE_STRING;
 
-    return fill(dateTemplate, dateComponents, transformMap as FillTransformMap);
+    return fill(dateTemplate, dateComponents, transformMap);
 }
