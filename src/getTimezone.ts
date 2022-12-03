be45ca8ea0449556a79406579cef8a18683b4ee7
axtk <axtkx@inbox.ru>
2022-12-03 12:00:49 +0300

    allow zero timezone format

--- src/getTimezone.ts
+++ src/getTimezone.ts
@@ -1,3 +1,9 @@
 export function getTimezone(dateString: string | undefined): string | undefined {
-    return dateString?.match(/[-\+]\d\d:?\d\d\b/)?.[0];
+    if (!dateString)
+        return;
+
+    if (/(^|T|\s)\d\d:\d\d:\d\d(\.\d{3})?Z(\s|$)/.test(dateString))
+        return 'Z';
+
+    return dateString.match(/[-\+]\d\d:?\d\d\b/)?.[0];
 }
